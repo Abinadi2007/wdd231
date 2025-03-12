b02a39a4c65c950751c8fe5ca9d871dfa82da39d
Abinadi2007 <josephabinadi@gmail.com>
2025-03-12 10:33:14 -0600

    modified:   index.html 	modified:   scripts/getdates.js 	modified:   style/base.css

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -3,3 +3,13 @@ document.getElementById("currentyear").textContent = currentYear;
 
 const lastModified = document.lastModified;
 document.getElementById("last-modified").textContent = lastModified;
+
+document.addEventListener("DOMContentLoaded", () => {
+    const menuBtn = document.getElementById("menu-toggle");
+    const menu = document.querySelector(".navbar");
+
+    menuBtn.addEventListener("click", () => {
+        menu.classList.toggle("open");
+        menuBtn.classList.toggle("open");
+    });
+});
